b87cb53649b095d82b7fb6f5fb68cbf9123142ad
abhayoffice <abhay.office0@gmail.com>
2024-02-05 12:58:47 +0530

    Monday commit

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ slack_event_adapter = SlackEventAdapter(
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")["user_id"]
 
+message_counts = {}
 # client.chat_postMessage(channel="#test", text = "Hello World!")
 
 @slack_event_adapter.on('message')
@@ -24,11 +25,28 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
+    # client.chat_postMessage(channel= channel_id, text=text)
 
     if BOT_ID != user_id:
-        client.chat_postMessage(channel= channel_id, text=text)
+        if user_id in message_counts:
+            message_counts[user_id]+=1
+        else:
+            message_counts[user_id]=1
+        # client.chat_postMessage(channel= channel_id, text=text)
 
 
+@app.route("/message_count", methods=['POST'])
+def message_count():
+    data = request.form
+    print(data)
+    user_id = data.get('user_id')
+    username = data.get('user_name')
+    channel_id = data.get('channel_id')
+    message_cnt = message_counts.get(user_id,0)
+    # if user_id in message_counts:
+    #     client.chat_postMessage(channel=channel_id, text = username)
+    client.chat_postMessage(channel=channel_id, text = f"The message count is : {message_cnt}")
+    return Response(), 200
 
 
 if __name__ == "__main__":
